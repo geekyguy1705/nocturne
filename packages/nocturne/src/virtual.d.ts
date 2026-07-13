9afc500bb26f0b3c3238d168f19f3fe5b76a607d
Abhishek <abhisheklaha199@gmail.com>
2026-07-13 15:40:13 +0530

    refactor(package): migrate all route files into nocturne package

--- packages/nocturne/src/virtual.d.ts
+++ packages/nocturne/src/virtual.d.ts
@@ -2,3 +2,19 @@ declare module "virtual:nocturne/config" {
   import type { NocturneConfig } from "./config/index.ts"
   export const siteConfig: NocturneConfig
 }
+
+interface SvgGlyph {
+  d: string
+}
+
+interface SvgPathEntry {
+  key: string
+  glyphs: SvgGlyph[]
+  totalWidth: number
+  height: number
+}
+
+declare module "virtual:nocturne/svg-paths" {
+  const svgPaths: SvgPathEntry[]
+  export default svgPaths
+}
